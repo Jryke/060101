464dae5538f1b0f5cc258bffdff9f3b05a795dee
Jryke <rykerson@gmail.com>
2019-09-02 13:14:26 +0700

    060802

--- src/components/Place.js
+++ src/components/Place.js
@@ -8,15 +8,22 @@ class Place extends React.Component {
 			'https://q-ak.bstatic.com/images/hotel/max1280x900/186/186223171.jpg',
 			'https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223174.jpg',
 			'https://r-ak.bstatic.com/images/hotel/max1280x900/186/186223178.jpg'
-		]
+		],
+		selectedImage: 'https://q-ak.bstatic.com/images/hotel/max1024x768/186/186223203.jpg'
+	}
+
+	replaceImage = (imgSrc) => {
+		this.setState({
+			selectedImage: imgSrc
+		})
 	}
 
 	render() {
 		return(
 			<>
 				<h1>Places Component</h1>
-				<div class='place-background large-background' style={{backgroundImage: `url(${this.state.images[0]})`}}></div>
-				{this.state.images.map(img => <div class='place-background small-background' style={{backgroundImage: `url(${img})`}}></div>)}
+				<div id='place-large-background' className='place-background large-background' style={{backgroundImage: `url(${this.state.selectedImage})`}} ></div>
+				{this.state.images.map((img, i) => <div key={i} id={`background${i}`} className='place-background small-background' style={{backgroundImage: `url(${img})`}} onClick={() => this.replaceImage(this.state.images[i])}></div>)}
 			</>
 		)
 	}
